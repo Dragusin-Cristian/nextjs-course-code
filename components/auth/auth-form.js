753dc97970343ae1e7a49dae25ca5a1b2d347691
Dragusin Cristian <dragusincristian609@gmail.com>
2023-05-07 19:07:29 +0300

    added signup and login

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,24 +1,69 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import { signIn } from "next-auth/client";
 import classes from './auth-form.module.css';
+import { redirect } from 'next/dist/next-server/server/api-utils';
 
 function AuthForm() {
   const [isLogin, setIsLogin] = useState(true);
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  async function createUser(email, password) {
+    const response = await fetch("/api/auth/signup", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.message || "Something went wrong!")
+    }
+    return data.message
+  }
 
   function switchAuthModeHandler() {
     setIsLogin((prevState) => !prevState);
   }
 
+  async function submitHandler(e) {
+    e.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    if (isLogin) {
+      const result = await signIn("credentials", {
+        redirect: false, // don't redirect if password is wrong
+        email: enteredEmail, // the credentials are exactely the ones used in "credentials" param from authorize function
+        password: enteredPassword
+      });
+
+      console.log(result);
+    } else {
+      try {
+        const result = await createUser(enteredEmail, enteredPassword)
+        console.log(result);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input type='email' id='email' required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input type='password' id='password' required ref={passwordInputRef} />
         </div>
         <div className={classes.actions}>
           <button>{isLogin ? 'Login' : 'Create Account'}</button>
---
+++ pages/api/auth/signup.js
@@ -0,0 +1,41 @@
+import { hashPassword } from "../../../lib/auth";
+import { connectToDb } from "../../../lib/db";
+
+async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return;
+  }
+
+  const data = req.body;
+  const { email, password } = data;
+
+  if (!email || !email.includes("@") || !password || password.trim().length < 7) {
+    res.status(422).json({ message: "Invalid credentials!" })
+    return;
+  }
+
+  const client = await connectToDb();
+
+  const db = client.db();
+
+  const existingUser = await db.collection("users").findOne({ email: email });
+
+  if (existingUser) {
+    res.status(422).json({ message: "User already exists!" });
+    client.close();
+    return;
+  }
+
+  const hashedPassword = await hashPassword(password);
+
+  const result = await db.collection("users").insertOne({
+    email: email,
+    password: hashedPassword
+  });
+
+  client.close();
+
+  res.status(201).json({ message: "Created user!" });
+}
+
+export default handler;
