4a63cc373f986cb071714bdea5a5ad3512516b04
Dragusin Cristian <dragusincristian609@gmail.com>
2023-05-08 17:06:30 +0300

    added navbar auth logic

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,8 +1,14 @@
 import Link from 'next/link';
-
+import { useSession, signOut } from 'next-auth/client';
 import classes from './main-navigation.module.css';
 
 function MainNavigation() {
+  const [session, loading] = useSession();
+
+  const logoutHandler = () => {
+    signOut(); // this returns a promise for when it's done, but we already use useSession() here
+  }
+
   return (
     <header className={classes.header}>
       <Link href='/'>
@@ -12,15 +18,15 @@ function MainNavigation() {
       </Link>
       <nav>
         <ul>
-          <li>
+          {!session && !loading && <li>
             <Link href='/auth'>Login</Link>
-          </li>
-          <li>
+          </li>}
+          {session && <li>
             <Link href='/profile'>Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          </li>}
+          {session && <li>
+            <button onClick={logoutHandler}>Logout</button>
+          </li>}
         </ul>
       </nav>
     </header>
